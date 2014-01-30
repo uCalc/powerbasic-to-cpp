d03927c11d4277d272908057cc6424311f7090f9
Daniel Corbier <github@ucalc.com>
2014-01-30 18:35:13 -0500

    Update pbstrings.h
    Added support for negative Length

--- convert/pbstrings.h
+++ convert/pbstrings.h
@@ -5,6 +5,24 @@
 //               ...
 //               ...
 
+#define FULL_STRING string::npos
+
+#define PB_LEFT(MainStr, n) ((n) == 0 ? "" : \
+   (n)>0 ? string(MainStr).substr(0, (n))    \
+         : string(MainStr).substr(0, string(MainStr).length()+(n)))
+
+#define PB_RIGHT(MainStr, n) ((n) == 0 ? "" :                        \
+   (n)>0 ? string(MainStr).substr(string(MainStr).length()-(n), (n)) \
+         : string(MainStr).substr(-(n), (n))
+
+#define PB_MID(MainStr, Start, Length) string(MainStr).substr(Start-1, Length)
+// Help needed here for negative Start & Length
+// Turns out there might be a conflict between string::npos and MID(..., ..., -1) (for Left$, Right$ too)
+// Negative args were not always a feature in PB
+// These extra lines might be removed if nobody cares
+
+#define PB_MID_REPLACE(MainStr, Start, Length) string(MainStr).replace((Start)-1, Length, MainStr)
+
 #define PB_INSTR(n, MainStr, MatchStr) ((n) == 0 ? 0 :  \
    (n)>0 ? string(MainStr).find(MatchStr, (n)-1)+1      \
          : string(MainStr).rfind(MatchStr, string(MainStr).length()+(n))+1)
