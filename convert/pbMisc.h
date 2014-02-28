37ad035f27107caf05a490df01d23ab78b7ee066
Daniel Corbier <github@ucalc.com>
2014-02-28 08:36:02 -0500

    Create pbMisc.h
    PB built-in string equates

---
+++ convert/pbMisc.h
@@ -0,0 +1,21 @@
+// Miscellaneous
+// Actual patterns that convert to these are in *.uc
+
+
+// PB string equates
+
+const string PB_NUL  = "\x00";
+const string PB_BEL  = "\a";
+const string PB_BS   = "\b";
+const string PB_TAB  = "\t";
+const string PB_LF   = "\n";
+const string PB_VT   = "\v";
+const string PB_FF   = "\f";
+const string PB_CR   = "\r";
+const string PB_CRLF = "\r\n";
+const string PB_EOF  = "\x1A;
+const string PB_ESC  = "\x1B";
+const string PB_SPC  = " ";
+const string PB_DQ   = "\"";
+const string PB_SQ   = "\'";
+const string PB_QCQ  = "\q,\q";
