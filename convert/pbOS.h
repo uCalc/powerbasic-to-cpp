12dc95b4e0f33a616493f432bbe8381ae8ca7674
Daniel Corbier <github@ucalc.com>
2014-03-06 11:32:57 -0500

    Update pbOS.h
    Added C++ code for Dir$()

--- convert/pbOS.h
+++ convert/pbOS.h
@@ -1,9 +1,23 @@
 // This file is for handling operating system dependent code that might not be directly
 // portable using the same method.
+// +++ These might eventually (maybe) be replaced with Boost C++ Libraries
 
 // The actual transforms that convert to this code are found in *.uc
 
 #include "windows.h"  // For now everything is Windows-specific
+
+WIN32_FIND_DATA FindFileData;
+HANDLE DirHandle = INVALID_HANDLE_VALUE;
+
+inline string PB_DIR(string file) {
+   DirHandle = FindFirstFile(file, &FindFileData);
+   return (DirHandle == INVALID_HANDLE_VALUE ? "" : FindFileData.cFileName);
+}
+
+inline string PB_DIR_NEXT() {
+   FindNextFile(DirHandle, &FindFileData);
+   return (DirHandle == INVALID_HANDLE_VALUE ? "" : FindFileData.cFileName);
+}
 
 inline BOOL PB_ISFILE(string FileName) {
    DWORD attr = GetFileAttributes(FileName.c_str());
@@ -15,3 +29,4 @@ inline string PB_EXE_FULL() { TCHAR fName[MAX_PATH]; GetModuleFileNameW(NULL, fN
 #define PB_EXE_NAMEX PB_EXE_FULL().substr(PB_EXE_FULL().rfind("\")+1)
 #define PB_EXE_PATH  PB_EXE_FULL().substr(PB_EXE_FULL().rfind("\"))
 #define PB_EXE_EXTN  PB_EXE_FULL().substr(PB_EXE_FULL().rfind("."))
+
