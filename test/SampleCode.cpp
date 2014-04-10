bcc52101462eefdc96d8f84a0d20c863230f3f4e
Daniel Corbier <github@ucalc.com>
2014-04-10 17:26:46 -0400

    Update SampleCode.cpp

--- test/SampleCode.cpp
+++ test/SampleCode.cpp
@@ -1,5 +1,5 @@
 // This file (SampleCode.cpp) was converted from SampleCode.Bas
-// with uCalc Transform 2.95 on 4/9/2014 5:23:10 PM using the Open Source 
+// with uCalc Transform 2.95 on 4/10/2014 5:25:32 PM using the Open Source 
 // PowerBASIC to C++ converter found at https://github.com/uCalc/powerbasic-to-cpp
 
 #include "pbOS.h"
@@ -488,10 +488,13 @@ void PrintTest()
    cout << endl << endl << endl << endl;
    cout << a << endl << b << endl << c << endl << d << endl;
    cout << PZONE << a << PZONE << b;
-   cout <<, a << endl << PZONE << b << c << PZONE << d << e << endl;
-   cout <<, a << endl;
-   cout << PZONE << PZONE << a << b << endl;
-   cout << PZONE << PZONE << PZONE << a << PZONE << b;
+   cout << PZONE << "" << a << endl << PZONE << b << c << PZONE << d << e << endl;
+   cout << PZONE << "" << a << endl;
+   cout << PZONE << "" << PZONE << a << b << endl;
+   cout << PZONE << "" << PZONE << "" << PZONE << a << PZONE << b;
+   cout << PZONE << "" << PZONE << "" << PZONE << "" << PZONE << "" << a << endl;
+   cout << PZONE << "" << PZONE << a << PZONE << "" << PZONE << "";
+   cout << PZONE << "" << PZONE << a << PZONE << "" << PZONE << "" << "" << endl;
    cout << a << PZONE << b << c << PZONE << d << e << PZONE << f;
    cout << a << b << PZONE << c << d << endl;
 }
