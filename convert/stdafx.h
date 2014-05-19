e013fd249b56af4e2e533a9b21fdd85b31d922d4
Daniel Corbier <github@ucalc.com>
2014-05-19 18:33:51 -0400

    Update stdafx.h

--- convert/stdafx.h
+++ convert/stdafx.h
@@ -10,7 +10,8 @@
 #include <tchar.h>
 
 // TODO: reference additional headers your program requires here
-#include <cmath>
+//#include <cmath>
+#include <mathimf.h> // Use this for Intel C++, or <cmath> otherwise
 #include <cstdlib>
 #include <vector>
 #include <cstdarg>
@@ -20,3 +21,5 @@
 #include <iostream>
 #include <iomanip>
 #include <fstream>
+
+// Intel's mathimf.h does not have overload issue that <cmath> with VC++ has for things like pow()
