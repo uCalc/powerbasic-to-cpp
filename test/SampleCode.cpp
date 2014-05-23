0fb62bc57b4330c29e388ca8833cbfd61a8838bf
Daniel Corbier <github@ucalc.com>
2014-05-23 18:21:37 -0400

    Update SampleCode.cpp

--- test/SampleCode.cpp
+++ test/SampleCode.cpp
@@ -1,5 +1,5 @@
 // This file (SampleCode.cpp) was converted from SampleCode.Bas
-// with uCalc Transform 2.96 on 05/22/14 using the Open Source 
+// with uCalc Transform 2.96 on 05/23/14 using the Open Source 
 // PowerBASIC to C++ converter found at https://github.com/uCalc/powerbasic-to-cpp
 
 // Standard lib headers used by PB to C++ are in sdtafx.h & pre-compiled
@@ -723,14 +723,14 @@ double DoMath()
       q--;
    }
    n = sqrt(x) * atan(y) + 2; //* x^2 + Tan(x^3-1)^2
-   pow(n == x, 2) + pow(y, 2);
+   n = pow(x, 2) + pow(y, 2);
    y = PB_FIX(3.14159);
    n = PB_FRAC(3.14159);
    n = __int64(3.14159) + short(3.14159) + PB_EXP10(3+2);
    n = PB_MAX(ARGCOUNT(5), (double)5.3, (double) 11/2, (double) 17, (double) 4, (double) PB_MIN(ARGCOUNT(4), (double)5, (double) 3.5, (double) -2, (double) 8/7));
    i = PB_MIN_INT(ARGCOUNT(7), (int)3, (int) -10, (int) 7, (int) -1, (int) PB_MAX_INT(ARGCOUNT(4), (int)3, (int) 1, (int) 7, (int) -5), (int) 23, (int) 11);
    i = PB_MIN_INT(ARGCOUNT(3), (int)1, (int) 2, (int) PB_MAX(ARGCOUNT(3), (double)5, (double) 6, (double) 7.5));
-   _round(i == 10, 3);
+   i = _round(10, 3);
    
    srand(1234);
    y = (rand() / RAND_MAX) + (rand() / RAND_MAX) + (rand() % (2014) + (1995));
